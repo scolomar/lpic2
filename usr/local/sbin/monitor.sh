d8f78c9d93b1e61c70bbf3f7b60b7b00c2050fdb
secobau <sebastian.colomar@gmail.com>
2019-10-16 13:47:40 +0200

    Update monitor.sh

--- usr/local/sbin/monitor.sh
+++ usr/local/sbin/monitor.sh
@@ -2,19 +2,22 @@
 set -e
 
 forks_max=900000
-cpu_max=99
+pcpu_max=99
 mem_min=100000
 blocks_min=1000000
 inodes_min=100000
 
-forks=$( vmstat -s | grep forks |awk -F' ' '{print $1}')
-cpu=$(ps -e -o pcpu|sort|tail -2|head -1|cut -d. -f1|cut -d' ' -f2)
-mem=$(free|grep cache:|awk '{print $4}')
-blocks=$(df|grep var|awk '{print $4}')
-inodes=$(df -i|grep var|awk '{print $4}')
-[ $forks -gt $forks_max ]&&/usr/local/sbin/scolomar-alarm.sh FORKS
-[ $cpu -gt $cpu_max ]&&/usr/local/sbin/scolomar-alarm.sh CPU
-[ $mem -lt $mem_min ]&&/usr/local/sbin/scolomar-alarm.sh MEM
-[ $blocks -lt $blocks_min ]&&/usr/local/sbin/scolomar-alarm.sh BLOCKS
-[ $inodes -lt $inodes_min ]&&/usr/local/sbin/scolomar-alarm.sh INODES
+# Google cloud shell
+forks=$( vmstat --forks | awk ' { print $1 } ' )
+pcpu=$( ps -e -o pcpu | sort | tail -n 2 | head -n 1 | awk ' { print $1 } ' )
+mem=$( free | awk ' /Mem/{ print $7 } ' )
+blocks=$( df | awk ' /home/{ print $4 } ' )
+inodes=$( df --inodes | awk ' /home/{ print $4 } ' )
+
+[ $forks -gt $forks_max ]   && /usr/local/sbin/alarm.sh FORKS
+[ $pcpu -gt $pcpu_max ]     && /usr/local/sbin/alarm.sh CPU
+[ $mem -lt $mem_min ]       && /usr/local/sbin/alarm.sh MEM
+[ $blocks -lt $blocks_min ] && /usr/local/sbin/alarm.sh BLOCKS
+[ $inodes -lt $inodes_min ] && /usr/local/sbin/alarm.sh INODES
+
 exit 0
