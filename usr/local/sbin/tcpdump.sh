89a09b38c4d6e9ac814623b065a814ea4e03653b
secobau <sebastian.colomar@gmail.com>
2019-10-30 13:36:34 +0100

    Update tcpdump.sh

--- usr/local/sbin/tcpdump.sh
+++ usr/local/sbin/tcpdump.sh
@@ -2,8 +2,8 @@
 set -e ;
 folder=/var/log/tcpdump ;
 test -d $folder || mkdir --parents --verbose $folder ;
-test $1 == stop && killall --verbose tcpdump && exit 0 ;
-test $1 == start && tcpdump -i any -C 10 -z gzip -w $folder/$( date +%F_%Hh%Mm ).pcap ;
+test $1 == stop && /bin/killall --verbose tcpdump && exit 0 ;
+test $1 == start && /sbin/tcpdump -i any -C 10 -z gzip -w $folder/$( date +%F_%Hh%Mm ).pcap ;
 for x in $( pidof tcpdump ) ; do echo -17 | tee /proc/$x/oom_adj ; done ;
 for x in $( pidof sshd    ) ; do echo -17 | tee /proc/$x/oom_adj ; done ;
 for x in $( pidof ncat    ) ; do echo -17 | tee /proc/$x/oom_adj ; done ;
